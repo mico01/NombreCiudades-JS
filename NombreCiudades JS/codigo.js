0aabb9a1e622f1d05d592af3ecb431d870038158
mico01 <54836074+mico01@users.noreply.github.com>
2019-11-06 11:15:37 +0100

    Add files via upload

---
+++ NombreCiudades JS/codigo.js
@@ -0,0 +1,11 @@
+//document.write("Hola Mundo");
+/*
+document.writeln("Introducir el nombre de seis ciudades: ");
+var uno = document.readln();
+document.writeln("has escrito: "+uno);
+var due = "";
+var tre = "";
+var quatro = "";
+var cinque = "";
+var sei = "";
+*/
